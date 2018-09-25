a3567a775864d047556807f0cc0a8cc574c8dc41
Leemet <33695127+markusleemet@users.noreply.github.com>
2018-09-25 10:16:03 +0300

    Algeline HTML koos inputi ja buttoniga

--- src/main/java/ee/ut/cs/wad2018/test2/service/DataService.java
+++ src/main/java/ee/ut/cs/wad2018/test2/service/DataService.java
@@ -4,7 +4,6 @@ import ee.ut.cs.wad2018.test2.entity.ActorEntity;
 import ee.ut.cs.wad2018.test2.repository.DataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
