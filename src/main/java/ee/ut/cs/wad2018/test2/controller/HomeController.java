562ef6bfa37a4144d64ded68a42f7f7ae02254a9
Ingo Ignašov <ingo.ignasov@gmail.com>
2018-09-28 13:06:13 +0300

    nupud tööle

--- src/main/java/ee/ut/cs/wad2018/test2/controller/HomeController.java
+++ src/main/java/ee/ut/cs/wad2018/test2/controller/HomeController.java
@@ -2,14 +2,20 @@ package ee.ut.cs.wad2018.test2.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
     @RequestMapping(path = "/test", method = RequestMethod.GET)
-    public String getTestPage(){
+    public String getTestPage() {
         return "home/index.html";
     }
 
-}
+
+    @PostMapping("/login")
+        public String getLoginPage () {
+            return "home/login.html";
+        }
+    }
