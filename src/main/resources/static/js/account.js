997ea269b386c51f645e6bc65d63f4fc1cf47a40
Leemet <33695127+markusleemet@users.noreply.github.com>
2018-10-03 14:05:44 +0300

    Google sign-in with Heroku

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -1,7 +1,10 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+
+    var name = document.getElementById("name");
+    //name.innerHTML = profile.getName();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
+    console.log('Name: ' + name);
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
