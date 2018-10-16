4dbec3db1a37bd94d0740a61f8e6f66624f6a444
Leemet <33695127+markusleemet@users.noreply.github.com>
2018-10-16 13:11:46 +0300

    Send token to server close #31

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -1,11 +1,8 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
 
-    var name = document.getElementById("name");
-    //name.innerHTML = profile.getName();
-
-
     var id = profile.getId();
+    var id_token = googleUser.getAuthResponse().id_token;
     var nimi = profile.getName();
     var email = profile.getEmail();
     var pilt = profile.getImageUrl();
@@ -26,11 +23,12 @@ function onSignIn(googleUser) {
     console.log(nimi);
     console.log(email);
     console.log(pilt);
+    console.log(id_token);
 
     $.ajax({
         url: '/addKasutaja',
         type: 'POST',
-        data: jQuery.param({id: id, nimi: nimi,email:email,pilt:pilt}),
+        data: jQuery.param({id: id_token, nimi: nimi,email:email,pilt:pilt}),
         success: function () {
             console.log("korras");
         },
@@ -39,4 +37,8 @@ function onSignIn(googleUser) {
             $("#result").html('Some kind of error :( !');
         }
     });
+}
+
+function tere(){
+    console.log("tere");
 }
