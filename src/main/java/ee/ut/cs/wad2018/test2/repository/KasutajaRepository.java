e5cdc6121e9d0368607c2140e6e2818ffeca6081
Ingo Ignašov <ingo.ignasov@gmail.com>
2018-10-16 12:11:44 +0300

    viga

--- src/main/java/ee/ut/cs/wad2018/test2/repository/KasutajaRepository.java
+++ src/main/java/ee/ut/cs/wad2018/test2/repository/KasutajaRepository.java
@@ -1,10 +1,10 @@
 package ee.ut.cs.wad2018.test2.repository;
 
-import org.postgresql.util.PSQLException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RestController;
 
 @Repository
 public class KasutajaRepository {
@@ -16,12 +16,8 @@ public class KasutajaRepository {
     }
 
     @Transactional
-    public int kasutajaUpdate(String nimi, String email, String pilt, String id) {
-        try {
-            return jdbcTemplate.update("INSERT INTO kasutajad(nimi,email,pilt,id) VALUES(?,?,?,?)", nimi, email, pilt, id);
-        } catch (Exception e) {
-            System.out.println(e + "VIGA");
-            return -1;
-        }
+    public int kasutajaUpdate(String nimi,String email,String pilt,String id) {
+
+        return jdbcTemplate.update("INSERT INTO kasutajad(nimi,email,pilt,id) VALUES(?,?,?,?)", nimi,email,pilt,id);
     }
 }
