ca3ff0ef7d5ec06786f9356855f25d3cf5e68ffb
Ingo Ignašov <ingo.ignasov@gmail.com>
2018-10-16 12:07:11 +0300

    proov

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -1,27 +1,27 @@
-function onSignIn() {
-    /var profile = googleUser.getBasicProfile();
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
 
     var name = document.getElementById("name");
     //name.innerHTML = profile.getName();
-    /*
+
 
     var id = profile.getId();
     var nimi = profile.getName();
     var email = profile.getEmail();
     var pilt = profile.getImageUrl();
 
-    /*
+
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-*/
 
+/*
     var id = "kala";
     var nimi = "on";
     var email = "sees@hotmai.com";
     var pilt  = "pilt";
-
+*/
     console.log(id);
     console.log(nimi);
     console.log(email);
