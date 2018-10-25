236de45d268973f8fec756ec179d36399fecfe98
Ingo Ignašov <ingo.ignasov@gmail.com>
2018-10-25 13:57:32 +0300

    Tegin valmis CDN failimise korral lokaalse Jquery kasutamise; close #22

--- src/main/java/ee/ut/cs/wad2018/test2/config/Config.java
+++ src/main/java/ee/ut/cs/wad2018/test2/config/Config.java
@@ -25,9 +25,9 @@ public class Config {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/raamatupood");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
         dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
+        dataSource.setPassword("hiljemmuudame");
         return dataSource;
 */
 
